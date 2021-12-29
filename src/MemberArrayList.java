e37bdad1893677fe322415a674e14afc0febd94d
ImNotAntMan <ikarusppong@gmail.com>
2021-12-29 17:53:12 +0900

    Update MemberArrayList.java

--- src/MemberArrayList.java
+++ src/MemberArrayList.java
@@ -1,19 +1,20 @@
 import java.util.*;
+
 public class MemberArrayList {
-	ArrayList<Member> arrayList;	// 멤버변수, 선언만
-	
+	ArrayList<Member> arrayList; // 멤버변수, 선언만
+
 	public MemberArrayList() {
-		arrayList = new ArrayList<Member>();	// 멤버 객체를 저장할 수 있는 리스트를 생성
+		arrayList = new ArrayList<Member>(); // 멤버 객체를 저장할 수 있는 리스트를 생성
 	}
-	
-	public void addMember(Member member) {	// 회원 추가
+
+	public void addMember(Member member) { // 회원 추가
 		arrayList.add(member);
 	}
-	
-	public boolean removeMember(int memberId) {	// 삭제 성공하면 true 반환
+
+	public boolean removeMember(int memberId) { // 삭제 성공하면 true 반환
 		int k = 0;
-		for(Member member : arrayList) {
-			if(member.getMenberId() == memberId) {
+		for (Member member : arrayList) {
+			if (member.getMenberId() == memberId) {
 				System.out.println(memberId + ":" + member.getMemberName() + "을(를) 삭제했습니다.");
 				arrayList.remove(k);
 				return true;
@@ -32,11 +33,11 @@ public class MemberArrayList {
 		System.out.println(memberId + "가(이) 존재하지 않습니다.");
 		return false;
 	}
-	
+
 	public boolean removeMember(String memberName) {
 		int k = 0;
-		for(Member member : arrayList) {
-			if(member.getMemberName().equals(memberName)) {
+		for (Member member : arrayList) {
+			if (member.getMemberName().equals(memberName)) {
 				System.out.println(member.getMenberId() + ":" + member.getMemberName() + "을(를) 삭제했습니다.");
 				arrayList.remove(k);
 				return true;
@@ -46,9 +47,9 @@ public class MemberArrayList {
 		System.out.println(memberName + "가(이) 존재하지 않습니다.");
 		return false;
 	}
-	
+
 	public void showAllMember() {
-		for(Member member : arrayList) {
+		for (Member member : arrayList) {
 			System.out.println(member);
 		}
 		System.out.println();
